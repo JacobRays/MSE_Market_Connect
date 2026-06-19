d98420641872bee2bf5838c4dc4f1923332fcee7
JacobRays <140740318+JacobRays@users.noreply.github.com>
2026-06-19 10:25:13 +0000

    Fix android/build.gradle.kts (plugins block + buildDir File)

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,19 +1,23 @@
-id("com.google.gms.google-services") version "4.4.1" apply false
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
-    }
+plugins {
+  // Must be inside plugins {} in Kotlin DSL
+  id("com.google.gms.google-services") version "4.4.1" apply false
 }
 
-rootProject.buildDir = "../build"
-subprojects {
-    project.buildDir = "${rootProject.buildDir}/${project.name}"
+allprojects {
+  repositories {
+    google()
+    mavenCentral()
+  }
 }
+
+// buildDir expects a File (not a String)
+rootProject.buildDir = file("../build")
+
 subprojects {
-    project.evaluationDependsOn(":app")
+  project.buildDir = file("${rootProject.buildDir}/${project.name}")
+  project.evaluationDependsOn(":app")
 }
 
 tasks.register<Delete>("clean") {
-    delete(rootProject.buildDir)
+  delete(rootProject.buildDir)
 }
